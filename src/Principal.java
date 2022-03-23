7e92130f1a0b95bbe01ba5f05c22b2a5ae714237
Jetmarte <obcollazos@gmail.com>
2022-03-22 20:32:02 -0400

    primer commit

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("saludos a todos mis fans");
+        System.out.println(showMeTheMoney());
+
+
+    }
+
+    private static String showMeTheMoney(){
+        return "SHOW ME THE MONEY";
+    }
+}
