61a0541886b81c42b980c5b4a628a9a94e73e355
jetmarteomar <obcrsoft@gmail.com>
2022-03-22 21:29:04 -0400

    se va la segundita

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("saludos a todos mis fans");
         System.out.println(showMeTheMoney());
+        System.out.println("this is a test of github");
 
 
     }
