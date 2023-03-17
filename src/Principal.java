8eceb4e8180e47854256db4516e51a94cf9a3c19
jetmarteomar <obcrsoft@gmail.com>
2023-03-16 23:41:26 -0400

    prueba automatizacon varias cuentas git

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
         System.out.println("saludos a todos mis fans");
         System.out.println(showMeTheMoney());
         System.out.println("this is a test of github");
-
+        System.out.println("jetmarteomar");
 
     }
 
